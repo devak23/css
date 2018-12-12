2c35b675d44e1336e46aefc235bd30e1a8a0ef8d
Abhay Kulkarni <developer.ak23@gmail.com>
2018-12-13 02:19:17 +0530

    Defined a basic grid

---
+++ css-grids/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="grid">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+      </div>
+    );
+  }
+}
+
+export default App;
