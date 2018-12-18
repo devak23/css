0d9c4b551c71a275d16d165c80fa7acd610050ea
devak23 <developer.ak23@gmail.com>
2018-12-18 16:55:45 +0530

    playing with floats

---
+++ floats/src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="box left">
+          <h2>First</h2>
+          <p>
+            Nulla cupidatat et reprehenderit aliqua esse officia est irure. Sit do et mollit mollit magna eu sit nostrud
+            Lorem. Tempor in velit ea magna. Eu amet et consequat et commodo. Et velit Lorem cillum velit ut Lorem eu
+            nulla mollit do aliquip voluptate commodo laborum. Exercitation voluptate non non quis reprehenderit
+            incididunt sunt cupidatat aliquip.
+          </p>
+        </div>
+        <div className="box left">
+          <h2>Second</h2>
+          <p>
+            Deserunt incididunt excepteur elit aliquip dolor culpa aliqua. Sit occaecat veniam mollit nostrud cillum
+            adipisicing nostrud consequat pariatur anim eu cupidatat voluptate. Culpa magna ad non eiusmod officia.
+            Voluptate laborum nostrud quis ut ea. Et fugiat reprehenderit aute in esse minim consequat in esse proident
+            irure proident consectetur. Ex laboris fugiat laborum cupidatat minim duis adipisicing ad anim voluptate
+            dolor labore. Veniam et excepteur proident pariatur ad. Aute eu quis reprehenderit duis. Enim ad
+            reprehenderit eu exercitation mollit irure aliquip exercitation deserunt amet. Proident ullamco pariatur
+            exercitation veniam aute aute esse anim dolore. Et commodo ut ad fugiat sint consequat sint ipsum.
+          </p>
+        </div>
+        <div className="box left">
+          <h2>Third</h2>
+          <p>
+            Cupidatat do deserunt pariatur adipisicing laborum. Ullamco voluptate pariatur Lorem culpa nostrud elit eu
+            deserunt enim cillum non ex. Quis non nulla irure do officia fugiat. Consectetur mollit deserunt fugiat
+            mollit id velit nostrud aliqua minim laborum laboris exercitation. Sunt adipisicing ad minim eiusmod
+            nostrud. In commodo proident aute eiusmod pariatur irure anim deserunt.
+          </p>
+        </div>
+        <div className="box left">
+          <h2>Fourth</h2>
+          <p>
+            Aliquip ut Lorem sit excepteur. In sint deserunt cupidatat mollit magna ex commodo magna officia aute. Eu
+            ullamco aliqua ipsum non ipsum dolore id irure aliquip Lorem officia. Dolore et velit nisi ipsum minim
+            nostrud ad sit adipisicing laborum. Tempor sunt qui veniam do adipisicing anim id adipisicing velit
+            exercitation. Deserunt tempor proident deserunt aute in incididunt eiusmod. Aute labore ut adipisicing
+            labore officia. Excepteur incididunt culpa minim est est adipisicing velit dolore exercitation elit anim.
+            Proident voluptate in cupidatat dolore laborum consectetur dolor sint sint. Et ad aute elit esse occaecat
+            anim proident amet. Adipisicing dolor occaecat culpa esse quis nulla aute duis officia non elit minim Lorem.
+            Occaecat nisi adipisicing dolor minim adipisicing ipsum cupidatat sit veniam ex ea minim. Culpa quis qui
+            dolor laborum enim amet voluptate quis enim. Excepteur nisi cillum enim ut. Adipisicing pariatur anim qui
+            dolor cillum cupidatat consectetur ipsum esse ex dolore deserunt veniam minim. Officia magna ullamco
+            voluptate voluptate voluptate consectetur aute pariatur.
+          </p>
+        </div>
+        <div className="clear" />
+      </div>
+    );
+  }
+}
+
+export default App;
