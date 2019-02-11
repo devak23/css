76277e646d895a2c3fc2af2713a177ed33458938
devak23 <developer.ak23@gmail.com>
2019-02-12 00:21:35 +0530

    added search auto complete options with images

--- travelville/index.js
+++ travelville/index.js
@@ -8,3 +8,16 @@ M.Slider.init(slider, {
   transition: 500,
   interval: 2000
 });
+
+const autocomplete = document.querySelector('.autocomplete');
+M.Autocomplete.init(autocomplete, {
+  data: {
+    Aruba: 'img/aruba_thumb.jpg',
+    'Cancun Mexico': 'img/cancun_thumb.jpg',
+    Hawaii: 'img/hawaii_thumb.jpg',
+    California: 'img/california_thumb.jpg',
+    Bahamas: 'img/bahamas_thumb.jpg',
+    Jamaica: 'img/jamaica_thumb.jpg',
+    Europe: 'img/europe_thumb.jpg'
+  }
+});
