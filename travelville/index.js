6ed3deb4dca575275f53a52b0e51c4b5f1c50afc
devak23 <developer.ak23@gmail.com>
2019-02-11 09:53:43 +0530

    added Slider

--- travelville/index.js
+++ travelville/index.js
@@ -1,2 +1,10 @@
 const sidenav = document.querySelector('.sidenav');
 M.Sidenav.init(sidenav, {});
+
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  transition: 500,
+  interval: 6000
+});
